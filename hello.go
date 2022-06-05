8f661e7e48afc3003e2c30809540d83a794fe2c1
parajuliswopnil <parajuliswopnil717@gmail.com>
2022-06-05 21:17:55 +0545

    added new function calls

--- hello.go
+++ hello.go
@@ -16,10 +16,22 @@ func main() {
 
 	// conditionals(8)
 
-	fibonacci(20)
+	// fibonacci(20)
 
 	// switchCase()
 
 	// array()
 	// slices()
+	// maps()
+	// rangeLearn()
+
+	// key, value := multipleReturn("swopnila")
+	// fmt.Println("", key, " ", value)
+
+	// numberSlice := []int{1,2,3,4,5}
+	// numberSlice = append(numberSlice, 6)
+	// variadicSumFunction(numberSlice...)
+
+	returnedFunction := closure()
+	fmt.Println(returnedFunction())
 }

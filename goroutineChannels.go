48f87aa5ab4f574879bac66f452e8c04bbc367c7
parajuliswopnil <parajuliswopnil717@gmail.com>
2022-06-13 23:04:03 +0545

    quiz game with timer

--- goroutineChannels.go
+++ goroutineChannels.go
@@ -8,6 +8,7 @@ import (
 type Questions struct{
 	question string
 	answer []string
+	correctAnswer string
 }
 
 func sendSomethingInChannel()int{
@@ -27,24 +28,41 @@ func sendSomethingInChannel()int{
 	return 0
 }
 
-func (q Questions)askQuestion()(string, []string){
-	return q.question, q.answer
+func (q Questions)askQuestion()(string, []string, string){
+	return q.question, q.answer, q.correctAnswer
+}
+
+func contains[T comparable](array []T, value T) bool{
+	for _, val := range array{
+		if val == value{
+			return true
+		}
+	}
+	return false
 }
 
 func demoChannel() {
-	questions1 := Questions{question: "Highest mountain?", answer: []string{"nepal", "india", "china"}}
-	questions2 := Questions{question: "Tallest animal?", answer: []string{"lion", "tiger", "giraffe"}}
-	questions3 := Questions{question: "Fastest animal?", answer: []string{"tiger", "cheetah", "rhino"}}
-	questions4 := Questions{question: "Largest animal?", answer: []string{"elephant", "snake", "whale"}}
+	questions1 := Questions{question: "Highest mountain?", answer: []string{"nepal", "india", "china"}, correctAnswer: "nepal"}
+	questions2 := Questions{question: "Tallest animal?", answer: []string{"lion", "tiger", "giraffe"}, correctAnswer: "giraffe"}
+	questions3 := Questions{question: "Fastest animal?", answer: []string{"tiger", "cheetah", "rhino"}, correctAnswer: "cheetah"}
+	questions4 := Questions{question: "Largest animal?", answer: []string{"elephant", "snake", "whale"}, correctAnswer: "elephant"}
 
 	questionList := []Questions{questions1, questions2, questions3, questions4}
 	channel := make(chan int)
 	for i := 0; i < len(questionList); i++{
 		questionToAsk := questionList[i]
-		q, a := questionToAsk.askQuestion()
+		q, a , cor:= questionToAsk.askQuestion()
 		fmt.Println(q)
 		fmt.Println(a)
-	
+		var answer string
+		fmt.Scan(&answer)
+		if contains(a, answer){
+			if (answer == cor){
+				fmt.Println("Correct!")
+			}else{
+				fmt.Println("Incorrect")
+			}
+		} 
 	for i := 0; i < 10; i--{
 		go func()  {
 			channel <- sendSomethingInChannel()
